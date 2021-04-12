c675d7ea8005f54f89f92eb4ae61eb4db42e0011
unknown <thiago.gomes013@gmail.com>
2021-04-12 20:36:45 -0300

    iniciando projeto de cálculo de salário

--- public/exercicio.php
+++ public/exercicio.php
@@ -8,31 +8,32 @@
     <title>Exercício</title>
     <link rel="icon" type="image/png" size="16x16" href="img/estacio.png"> <!-- icone no browser -->
 </head>
+
 <body class="exercicio">
-     <header class="cabecalho">
+
+    <header class="cabecalho">
         <h1></h1>
         <h2>Realizando o cálculo</h2>
     </header> 
+
     <nav class="navegacao"> 
         <a href=<?= "/{$_GET['dir']}/{$_GET['file']}.php" ?> 
          class="verde"> Sem formatação</a>
         <a href="index.php" class="vermelho"> Voltar</a>
     </nav>
+
     <main class="principal">
         <div class="conteudo">
             <?php
                 include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php");
             ?>
-
         </div>
-       
     </main>
+
     <footer class="rodape">
         COD3R & ALUNOS © <?= date('Y'); ?>
-    </footer>
-
-    
-
+    </footer>  
 
 </body>
+
 </html>
